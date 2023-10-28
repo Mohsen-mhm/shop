70627c98a0aa67342634caabd6cd1323a4c236c7
Mohsen Mohammadi <Mohsen.mhm23@gmail.com>
2023-10-28 19:36:20 +0330

    Minor bugs fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,8 +21,8 @@
                                      alt="product image">
                             </div>
                             <div class="card-body p-4 pt-2 text-center text-light">
-                                <h5 class="pb-2">{{ $product->title }}</h5>
-                                <p style="height: 48px" class="overflow-hidden">{{ $product->description }}</p>
+                                <h5 class="pb-2 border-bottom"><b>{{ $product->title }}</b></h5>
+                                <p style="height: 48px" class="overflow-hidden border-bottom">{{ $product->description }}</p>
                                 <div class="d-flex justify-content-around">
                                     <p>قیمت:</p>
                                     <b>{{ $product->price }}</b>
